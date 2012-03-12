40d57e92513161bac88cfac2219f324bd01f9636
James Pleasant <James.M.Pleasant@gmail.com>
2012-03-13 03:16:27 +0900

    User model with passwords, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,16 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @title = @user.name
   end
   
   def new
     @title = "Sign up"
   end
+  
+  def new
+    @user = User.new
+    @title = "Sign up"
+  end
+  
 end
